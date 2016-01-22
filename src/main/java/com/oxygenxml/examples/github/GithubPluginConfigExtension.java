d94111d300f45fc4d209f5ab8ca70ee7f382d614
Gabriel Titerlea <titergab@yahoo.com>
2016-01-22 13:29:58 +0200

    WA-363: Moved the apiurl input below the instructions

--- src/main/java/com/oxygenxml/examples/github/GithubPluginConfigExtension.java
+++ src/main/java/com/oxygenxml/examples/github/GithubPluginConfigExtension.java
@@ -153,11 +153,6 @@ public class GithubPluginConfigExtension extends PluginConfigExtension {
               
             + "<form style='font-weight:bold;line-height:1.5em;margin-top:6px;'>"
               
-              + "<label id='gh-api-url' style='margin-top:6px;margin-bottom:6px;display:block;overflow:hidden;" + (apiUrl.isEmpty() ? "display:none;'" : "' class='using-enterprise'") + ">"
-                + "API URL: <span style='font-weight:normal;font-size:0.85em'>(http://enterprise-example.com)</span>"
-                + "<input placeholder='Your GitHub Enterprise deployment URL' name='" + API_URL + "' type='text' style='color:#606060;background-color:#FAFAFA;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;width:100%;border-radius:4px;border:1px solid #C8C1C1;padding:6px 4px' value='" + apiUrl +"'/>"
-              + "</label>"
-
               + "<div id='using-enterprise-msg' style='" + (apiUrl.isEmpty() ? "display:none;" : "display:block;") + "font-weight:normal;color:#333;background-color: lightyellow;border: 1px solid #dadab4;padding:0 5px 0 5px'>"
                 + "<p style='margin:5px 0'>The GitHub Plugin uses OAuth 2.0 to gain access to your files.</p>"
                 + "<p style='margin:5px 0'>To obtain the configuration parameters listed below go to your GitHub Enterprise deployment in settings > applications > developer applications (http://enterprise-example.com/settings/applications/new) and register a new application.</p>"
@@ -172,6 +167,11 @@ public class GithubPluginConfigExtension extends PluginConfigExtension {
                 + "<p style='margin:5px 0'><b>Example:</b><br/><span style='font-family:monospace'><span style='color:#D61564'>http://example.com/oxygen-web-author</span>/plugins-dispatcher/github-oauth/callback</span></p>"
               + "</div>"
             
+              + "<label id='gh-api-url' style='margin-top:6px;margin-bottom:6px;display:block;overflow:hidden;" + (apiUrl.isEmpty() ? "display:none;'" : "' class='using-enterprise'") + ">"
+                + "API URL: <span style='font-weight:normal;font-size:0.85em'>(http://enterprise-example.com)</span>"
+                + "<input placeholder='Your GitHub Enterprise deployment URL' name='" + API_URL + "' type='text' style='color:#606060;background-color:#FAFAFA;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;width:100%;border-radius:4px;border:1px solid #C8C1C1;padding:6px 4px' value='" + apiUrl +"'/>"
+              + "</label>"
+              
               + "<label style='margin-top:6px;display:block;overflow:hidden'>"
                 + "Client ID: "
                 + "<input placeholder='Client ID' name='" + CLIENT_ID + "' type='text' style='color:#606060;background-color:#FAFAFA;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box;width:100%;border-radius:4px;border:1px solid #C8C1C1;padding:6px 4px' value='" + clientId + "'/>"
